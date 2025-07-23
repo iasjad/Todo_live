214fcdea09c1c861c299e050d16bd618fa3bd6c7
Asjad <asjad673@gmail.com>
2025-07-24 00:55:55 +0530

    Fixing Bug

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'todo-live.vercel.app'],
+    origin: ['http://localhost:5173', 'https://todo-live.vercel.app'],
     methods: ['GET', 'POST'],
   },
 });
